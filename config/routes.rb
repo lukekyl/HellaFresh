9abc24d7584fa24bc98c90d4bfdc5fe6db8d3f41
Kyle Luke <luke.kylew@gmail.com>
2020-07-09 21:01:41 -0600

    Heroku Deployment Setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root :to => 'application#index'
+  root :to => '#index'
 
   resources :products
   resources :join_products, only: [:index, :show, :create, :destroy]
