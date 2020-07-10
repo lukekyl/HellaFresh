66a396ae88db84a94f0ae0a7ba230fed83c4193b
Kyle Luke <luke.kylew@gmail.com>
2020-07-09 20:51:03 -0600

    Heroku Deployment Setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+
+    def index
+        redirect_to :file => 'frontend/index.html'
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root :to => 'application#index'
+
   resources :products
   resources :join_products, only: [:index, :show, :create, :destroy]
   resources :carts, only: [:index, :show, :create]
